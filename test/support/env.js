ceb8ba97ac93fbd9b73a61b31b62d2967825e1b0
Chia Yu Pai <fantasyatelier@gmail.com>
2014-08-13 04:14:00 +0800

    update test mongo

--- test/support/env.js
+++ test/support/env.js
@@ -1,11 +1,6 @@
 process.env.NODE_ENV = 'test';
-process.env.TEST_MONGO = 'mongodb://test:test@localhost/test';
+process.env.TEST_MONGO = 'mongodb://test:test@1.34.9.252/test';
 process.env.SESSION_SECRET = 'sEcReT4un1ttest';
 process.env.FB_APP_ID = "287808734725360";
 process.env.FB_APP_SECRET = "10211b89fefc64669bc4930182e3ef28";
 process.env.FB_REDIRECT_URL = "http://sys.infinitibeat.com/auth/facebook/callback";
-process.env.MONGO_USER = "styletrip"
-process.env.MONGO_PASS = "infinitiBeat2O13"
-process.env.MONGO_DATABASE = "styletrip"
-process.env.MONGO_HOST = "1.34.9.252"
-process.env.MONGO_PORT = 27017
